5b47bb76024e50367842adddcae6e16736b42094
rkiko <rkiko@geomar.de>
2022-05-11 18:21:49 +0200

    included ecopart size bin definition

--- scripts/Zooscan_size_spectrum_calc.py
+++ scripts/Zooscan_size_spectrum_calc.py
@@ -10,6 +10,16 @@ import yaml # requires installation of PyYAML package
 
 import pandas as pd
 
+#size bin definition from ecopart
+
+#please check if these limits would work across instruments ...
+
+PartDetClassLimit = [0.001000, 0.001260, 0.001590, 0.002000, 0.002520, 0.003170, 0.004000, 0.005040, 0.006350, 0.008000
+    , 0.0101, 0.0127, 0.0160, 0.0202, 0.0254, 0.0320, 0.0403, 0.0508, 0.0640, 0.0806
+    , 0.1020, 0.1280, 0.1610, 0.2030, 0.2560, 0.3230, 0.4060, 0.5120, 0.6450, 0.8130
+    , 1.0200, 1.2900, 1.6300, 2.0500, 2.5800, 3.2500, 4.1000, 5.1600, 6.5000, 8.1900
+    , 10.3000, 13.0000, 16.4000, 20.6000, 26.0000, 10E7]
+
 
 ## Workflow starts here
 
