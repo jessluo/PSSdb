7546a8dee2f3faf687516984d0093b49f3fb9e8d
MarCorralesU <marco.corrales@noaa.gov>
2022-05-31 14:56:26 -0400

    played a bit with data distributions to figure out why we get bad linear fits

--- scripts/Size_spectra_functions.py
+++ scripts/Size_spectra_functions.py
@@ -15,13 +15,25 @@ import yaml  # requires installation of PyYAML package
 from scipy.stats import linregress
 
 
-# follow these to test functionality
-# path_to_data, ID = proj_id_list('IFCB', testing=True)
-# data_clean = read_clean(path_to_data, 3326)
-# data_clean = biovol(data_clean, 'IFCB')
-# data_clean = binning(data_clean)
-# stats_biovol_SC = NB_SS(data_clean)
-# results_SS = NB_SS_regress(stats_biovol_SC, 3326)
+# follow these to test functionality (this is to figure out why we are getting such horrible slopes)
+path_to_data, ID = proj_id_list('IFCB', testing=True)
+data_clean_all=pd.DataFrame()
+for i in ID:
+    data_clean = read_clean(path_to_data, i)
+    data_clean_all = pd.concat([data_clean_all, data_clean], axis=0)
+
+data_clean_all = biovol(data_clean_all, 'IFCB')
+
+data_clean_all['biovol_um3']=np.random.poisson(data_clean_all['biovol_um3'].mode(),
+                                               data_clean_all['biovol_um3'].count())
+
+data_clean_all = binning(data_clean_all)
+stats_biovol_SC = NB_SS(data_clean_all)
+
+results_SS_all=pd.DataFrame()
+for i in ID:
+    results_SS = NB_SS_regress(stats_biovol_SC, i)
+    results_SS_all = pd.concat([results_SS_all , results_SS], axis=0)
 
 
 # functions here
