392af56d7f155ede4c5048599669d42b9483b59c
MarCorralesU <marco.corrales@noaa.gov>
2022-06-06 13:21:00 -0400

    added some stuff to test functions 6 & 7

--- scripts/func_testing_PSSplotting.py
+++ scripts/func_testing_PSSplotting.py
@@ -7,6 +7,8 @@ for i in ID:
     data_clean = read_clean(path_to_data, i)
     data_clean_all = pd.concat([data_clean_all, data_clean], axis=0)
 
+# function that calls on the standardizer here
+
 data_clean_all['biovol_um3']= biovol_standardizer(data_clean_all['object_biovolume'],
                                      data_clean_all['process_pixel_to_micron'],  'IFCB')
 
@@ -19,13 +21,30 @@ data_clean_all['Station_ID'], data_clean_all['midLatBin'], data_clean_all['midLo
 #data_clean_all['biovol_um3']=np.random.poisson(data_clean_all['biovol_um3'].mode(),
                                                #data_clean_all['biovol_um3'].count())
 
-stats_biovol_SC = NB_SS(data_clean_all)
+stats_biovol_SC = NB_SS(data_clean_all,
+          station='Station_ID',
+          depths='midDepthBin',
+          size_range='range_size_bin',
+          sizeClasses='sizeClasses',
+          biovolume='biovol_um3',
+          lat='midLatBin',
+          lon='midLatBin',
+          project_ID='proj_ID')
+
+results_SS_all=pd.DataFrame()
+for i in ID:
+    results_SS = NB_SS_regress(stats_biovol_SC, station='Station_ID',
+                               depths='midDepthBin', lat='midLatBin', lon='midLonBin',  ID=i)
+    results_SS_all= pd.concat([results_SS_all, results_SS], axis=0)
+
+
 
 #plotting of biovolume and NBSS here. this is to see how the data looks like
 
 #first, all data
 plt.plot(stats_biovol_SC['logSize'], stats_biovol_SC['logNBSS']) #def not linear :O
 
+# NOTE: this can be useful to detect stations with problematic data
 #now, parse by project and by stations:
 stats_biovol_SC['proj_ID_min'] = stats_biovol_SC['proj_ID_min'].astype('int')
 stats_biovol_SC['proj_ID_min'] = stats_biovol_SC['proj_ID_min'].astype('category')
