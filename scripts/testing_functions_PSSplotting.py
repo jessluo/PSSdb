54f9faf91b6f1285f745d323b41a2e871e07eeb4
MarCorralesU <marco.corrales@noaa.gov>
2022-08-01 17:10:32 -0400

    added code to obtain all the graphs

--- scripts/testing_functions_PSSplotting.py
+++ scripts/testing_functions_PSSplotting.py
@@ -8,37 +8,38 @@ path_to_binned_IFCB, ID_list_IFCB =  proj_id_list_func('IFCB', standardized='bin
 path_to_binned_Zooscan, ID_list_Zooscan =  proj_id_list_func('Zooscan', standardized='binned', testing=False)
 
 # edit the list of projects, this is hardcoded and was obtained comparing dictionaries with the 'draft_funcs_merge'
-IFCB_list = [3302, 3309, 3321, 3324, 3325, 3334]
+IFCB_list_lgsample = [3302, 3309, 3321, 3324, 3325, 3334]
+IFCB_list_smsample = [3147, 2248, 3289, 3296]
 Zooscan_list = [378]
 
 #read the IFCB tsv's, clean them and plot them:
 path_to_IFCB = glob(str(path_to_binned_IFCB) + '/' + str(3309) + '*NBSS*') # for 3309, there is an issue in a single point where comparing NBSS between size bins does not work for filtering the shorter side of the spectrum
 binned_data_IFCB = pd.read_csv(path_to_IFCB[0], sep='\t', header=0, index_col=[0])
-binned_data_IFCB  = clean_lin_fit(binned_data_IFCB, instrument='IFCB')
+binned_data_IFCB  = clean_lin_fit(binned_data_IFCB, instrument='IFCB', method = 'instrument_specific')
 plt.plot(binned_data_IFCB ['logSize'], binned_data_IFCB ['logNBSS'])
 plt.xlabel('log (Biovolume)')
 plt.ylabel('log (NBS)')
-plt.title('IFCB 3309 WITH threshold')
+plt.title('IFCB 3309 with lower threshold defined by instrument specific threshold')
 
 
 #try to make a for loop to read all dataframes at the same time
 path_to_Zooscan = glob(str(path_to_binned_Zooscan) + '/' + str(378) + '*NBSS*') # for 3309, there is an issue in a single point where comparing NBSS between size bins does not work for filtering the shorter side of the spectrum
-IFCB_list = [str(x) for x in IFCB_list]
+IFCB_list_lgsample = [str(x) for x in IFCB_list_lgsample]
 IFCB_dict = {}
-for i in IFCB_list:
+for i in IFCB_list_lgsample:
     path_to_IFCB = glob(str(path_to_binned_IFCB) + '/' + i + '*NBSS*')
     IFCB_dict[i] = pd.read_csv(path_to_IFCB[0], sep='\t', header=0, index_col=[0])
-    IFCB_dict[i] = clean_lin_fit(IFCB_dict[i], instrument='IFCB')
+    IFCB_dict[i] = clean_lin_fit(IFCB_dict[i], instrument='IFCB', method= 'MAX')
     binned_data_Zooscan = pd.read_csv(path_to_Zooscan[0], sep='\t', header=0, index_col=[0])
     subset_binned_Zooscan = binned_data_Zooscan.loc[(binned_data_Zooscan['Station_location'] == IFCB_dict[i].loc[0, 'Station_location'])]
     subset_binned_Zooscan = subset_binned_Zooscan.sort_values(by='range_size_bin')
     subset_binned_Zooscan = subset_binned_Zooscan.reset_index(drop=True)
-    data_filt_Zooscan = clean_lin_fit(subset_binned_Zooscan, instrument='Zooscan')
+    data_filt_Zooscan = clean_lin_fit(subset_binned_Zooscan, instrument='Zooscan', method= 'instrument_specific')
     IFCB_dict[i] = pd.concat([IFCB_dict[i], data_filt_Zooscan], axis=0)
     IFCB_dict[i] = IFCB_dict[i].reset_index(drop=True)
 
 Station_list = []
-for i in IFCB_list:
+for i in IFCB_list_lgsample:
     Station_list.append(IFCB_dict[i].loc[0, 'Station_location'])
     station = (IFCB_dict[i].loc[0, 'Station_location'])
     IFCB_dict[station] = IFCB_dict.pop(i)
@@ -50,19 +51,20 @@ for st in Station_list:
 ax1.set_ylabel('log (NBS)')
 ax1.set_xlabel('log (Biovolume)')
 ax1.legend()
-ax1.title.set_text('NBSS for IFCB and Zooscan for six stations sampled on  7/2013')
+ax1.title.set_text('NBSS for IFCB (Max) and Zooscan (isntrument) for six stations sampled on  7/2013')
 
 # read the Zooscan data
+path_to_Zooscan = glob(str(path_to_binned_Zooscan) + '/' + str(378) + '*NBSS*')
 binned_data_Zooscan = pd.read_csv(path_to_Zooscan[0], sep='\t', header=0, index_col=[0])
 #subset zooscan data based on IFCB's station location
 subset_binned_Zooscan = binned_data_Zooscan.loc[(binned_data_Zooscan['Station_location'] == binned_data_IFCB.loc[0, 'Station_location'])]
 subset_binned_Zooscan = subset_binned_Zooscan.sort_values(by='range_size_bin')
 subset_binned_Zooscan= subset_binned_Zooscan.reset_index(drop=True)
-data_filt_Zooscan = clean_lin_fit(subset_binned_Zooscan, instrument='Zooscan')
+data_filt_Zooscan = clean_lin_fit(subset_binned_Zooscan, instrument='Zooscan', method = 'instrument_specific')
 plt.plot(data_filt_Zooscan['logSize'], data_filt_Zooscan['logNBSS'])
 plt.xlabel('log (Biovolume)')
 plt.ylabel('log (NBS)')
-plt.title('Zooscan 378, Station 79.5_66.5  WITH threshold')
+plt.title('Zooscan 378, Station 79.5_66.5 with instrument specific threshold')
 
 
 ## MERGE both datasets:
