05d4a5b61fe261ae79b952725bf5d9473d828ae4
mdugenne <mdugenne@hotmail.com>
2022-09-21 13:55:37 +0200

    Trying with selenium to get javascript content

--- scripts/IFCB_dashboard_metadata.py
+++ scripts/IFCB_dashboard_metadata.py
@@ -1,13 +1,31 @@
 
-import requests
-from bs4 import BeautifulSoup
 
+# Module for web crawling with javascript inputs
+# Selenium for web scraping: https://stackoverflow.com/questions/29385156/invoking-onclick-event-with-beautifulsoup-python
+# Download chromedriver at:https://chromedriver.chromium.org/downloads
+# Go in System Preferences > Security and Privacy > General > Allow
+from selenium import webdriver # Use: pip3 install -U selenium
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager # Use: pip3 install webdriver-manager
+from selenium.webdriver.chrome.options import Options
+chromedriver = '/Users/dugennem/Downloads/chromedriver' # Local path to the chromedriver. Note: Deprecated
+options=Options()
+options.add_argument("--headless") # Use this to keep the browser session closed
+
+import re
+import pandas as pd
+
+import warnings
+warnings.filterwarnings("ignore")
+
+import time
 url = "https://ifcb.caloos.org/timeline?dataset=santa-cruz-municipal-wharf&bin=D20160513T221410_IFCB104"
-page = requests.get(url)
 
-soup = BeautifulSoup(page.content, "html.parser")
-
-vol_analyzed = soup.find('span', {'id': 'stat-ml-analyzed'}).text
-depth = soup.find('span', {'id': 'stat-depth'}).text
-Latitude = soup.find('span', {'id': 'stat-lat'}).text
-Longitude = soup.find('span', {'id': 'stat-lon'}).text
+with webdriver.Chrome(chromedriver, options=options,keep_alive=False) as driver:
+    driver.get(url)
+    time.sleep(3) # Time needed for the webpage to be fully loaded, in seconds
+    vol_analyzed=float(re.sub("[^\d\.]", "",driver.find_element(by='id', value='stat-ml-analyzed').text)) if len(driver.find_element(by='id', value='stat-ml-analyzed').text)>0 else pd.NA
+    depth = float(re.sub("[^\d\.]", "", driver.find_element(by='id', value='stat-depth').text)) if len(driver.find_element(by='id', value='stat-depth').text)>0 else pd.NA
+    Latitude = float(driver.find_element(by='id', value='stat-lat').text) if len(driver.find_element(by='id', value='stat-lat').text)>0 else pd.NA
+    Longitude = float(driver.find_element(by='id', value='stat-lon').text) if len(driver.find_element(by='id', value='stat-lon').text)>0 else pd.NA
+    driver.quit()
