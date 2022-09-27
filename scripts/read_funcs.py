6cdf5bacd7f030cce7aeadfa0a8948077a03dafe
MarCorralesU <marco.corrales@noaa.gov>
2022-09-27 17:08:31 -0400

    fuctions for step3 script

--- scripts/read_funcs.py
+++ scripts/read_funcs.py
@@ -1,5 +1,8 @@
 
 # 2) function to create list of projects. Inputs: instrument
+import os
+
+
 def proj_id_list_func(instrument, data_status):
     """
     Objective:
@@ -23,6 +26,8 @@ def proj_id_list_func(instrument, data_status):
         path_to_data = Path(cfg['git_dir']).expanduser() / cfg['standardized_subdir'] / instrument
     elif data_status == 'gridded':
         path_to_data = Path(cfg['git_dir']).expanduser() / cfg['standardized_subdir'] / instrument / cfg['gridded_subdir']
+    elif data_status == 'NBSS':
+        path_to_data = Path(cfg['git_dir']).expanduser() / cfg['standardized_subdir'] / instrument / cfg['gridded_subdir'] / 'NBSS_data'
     # create a list  projects that we have access to, based on project_list_all.xlsx
     path_to_proj_id_list = Path(cfg['git_dir']).expanduser() / cfg['proj_list']
     proj_list = pd.read_excel(path_to_proj_id_list)
@@ -50,3 +55,44 @@ def read_func(path_to_data, ID):
     #df.loc[:, cat].astype('category')
     #df['proj_ID'] = [ID] * (len(df))
     return df
+
+
+def df_list_func(instrument, data_status):
+    """
+    Objective:
+    generate a list and create the path  of the standardized Ecotaxa projects
+    :param instrument: the device used for image adcquisition. important since the path will change according to it
+    :return path_to_data: string where the tsv files are stores
+    :return id_list: list with the ecotaxa project identifiers (might not be relevant once we expand
+    to get data from other platforms)
+    """
+    import pandas as pd
+    import yaml
+    from pathlib import Path
+    # returns path to project data and list of projects based on intrument (IFCB, Zooscan, UVP)
+    # read git-tracked config file (text file) with inputs:  project ID, output directory
+    path_to_config = Path('~/GIT/PSSdb/scripts/Ecotaxa_API.yaml').expanduser()
+    with open(path_to_config, 'r') as config_file:
+        cfg = yaml.safe_load(config_file)
+    # read config file (text file) with inputs:  project ID, output directory
+    # prepare storage based on path stored in the yaml config file and instrument type
+    if data_status == 'standardized':
+        path_to_data = Path(cfg['git_dir']).expanduser() / cfg['standardized_subdir'] / instrument
+        df_list = os.listdir(path_to_data)
+        df_list = [x for x in df_list if 'metadata' not in x]
+        df_list = [x for x in df_list if '.tsv' in x]
+        df_list = [str(path_to_data) +'/'+ x for x in df_list]
+    elif data_status == 'gridded':
+        path_to_data = Path(cfg['git_dir']).expanduser() / cfg['standardized_subdir'] / instrument / cfg['gridded_subdir']
+        df_list = os.listdir(path_to_data)
+        df_list = [x for x in df_list if 'metadata' not in x]
+        df_list = [x for x in df_list if '.tsv' in x]
+        df_list = [str(path_to_data) +'/'+ x for x in df_list]
+    elif data_status == 'NBSS':
+        path_to_data = Path(cfg['git_dir']).expanduser() / cfg['standardized_subdir'] / instrument / cfg['gridded_subdir'] / 'NBSS_data'
+        df_list = os.listdir(path_to_data)
+        df_list = [x for x in df_list if 'metadata' not in x]
+        df_list = [x for x in df_list if '.tsv' in x]
+        df_list = [str(path_to_data) +'/'+ x for x in df_list]
+
+    return path_to_data, df_list
