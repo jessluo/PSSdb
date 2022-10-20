9d36414ec0d770cc2a8a4155998cd31598b76143
MarCorralesU <marco.corrales@noaa.gov>
2022-10-20 18:58:13 -0400

    added Karinas functions to add metadata to the features files, each file download takes betwee 3-5 seconds

--- scripts/download_IFCB_dashboard.py
+++ scripts/download_IFCB_dashboard.py
@@ -1,4 +1,5 @@
 import datetime as dt
+import re
 import requests
 import urllib.request
 import pandas as pd
@@ -9,41 +10,50 @@ import numpy as np
 import yaml
 from pathlib import Path
 import re
-# Module for web crawling with javascript inputs
-# Selenium for web scraping: https://stackoverflow.com/questions/29385156/invoking-onclick-event-with-beautifulsoup-python
-# Download chromedriver at:https://chromedriver.chromium.org/downloads
-# Go in System Preferences > Security and Privacy > General > Allow
-from selenium import webdriver # Use: pip3 install -U selenium
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager # Use: pip3 install webdriver-manager
-from selenium.webdriver.chrome.options import Options
-import warnings
-warnings.filterwarnings("ignore")
 
-import time
-
-chromedriver = '{}/Downloads/chromedriver'.format(str(Path.home())) # Local path to the chromedriver. Note: Deprecated
-options=Options()
-options.add_argument("--headless") # Use this to keep the browser session closed
-
-# set paths, dates for query, and dataset name:
+# set paths:
 path_to_config = Path('~/GIT/PSSdb/scripts/Ecotaxa_API.yaml').expanduser()
 # open the metadata of the standardized files
 with open(path_to_config, 'r') as config_file:
     cfg = yaml.safe_load(config_file)
 IFCB_dashboard_data_path = str(Path(cfg['IFCB_dir']).expanduser())
 
+
+# Set names of variables (date range, dataset, dashboard name)
 y = {}
-y['startdate']=dt.datetime(2015,8,21);
-y['enddate']=dt.datetime(2015,9,10);
-y['size_cutoff']=50;
-dataset_name = 'santa-cruz-municipal-wharf'
-y['path_download']= IFCB_dashboard_data_path + '/' + dataset_name ## NOTE: the first part of the directory needs to be changed for the GIT PSSdb project
-pathname = 'https://ifcb.caloos.org/' + dataset_name + '/'
+y['startdate'] = dt.datetime(2015,8,21);
+y['enddate'] = dt.datetime(2015,9,10);
+y['size_cutoff'] = 50;
+Project_ID = 'santa-cruz-municipal-wharf'
+Project_source = 'https://ifcb.caloos.org/'
+BASE_URL=Project_source # needs to be defined based on Karina's script
+METRIC = 'temperature' # needs to be defined based on Karina's script
+y['path_download'] = IFCB_dashboard_data_path + '/' + Project_ID ## NOTE: the first part of the directory needs to be changed for the GIT PSSdb project
+pathname = Project_source + Project_ID  + '/'
 
 if os.path.exists(y['path_download']) and os.path.isdir(y['path_download']):
     shutil.rmtree(y['path_download'])
 os.mkdir(y['path_download'])
+
+## FUNCTIONS HERE
+
+## create list of dates available for the project of interest
+def get_df_list_IFCB (base_url = Project_source, dataset = Project_ID):
+    link = base_url + 'api/list_bins?dataset=' + dataset # this path is for the data LIST only
+    #metadata_link = "https://ifcb.caloos.org/timeline?dataset="
+    r = requests.get(link)
+    r_content = r.content
+    r_content = json.loads(r_content.decode('utf-8'))
+    all_file_info = pd.DataFrame.from_dict(r_content['data'], orient = 'columns')
+    all_file_info['date_info'] = [int(sample[1:9]) for sample in all_file_info['pid']]
+    # constrain date ranges for file download
+    start_stamp=int(y['startdate'].strftime('%Y%m%d'))
+    end_stamp=int(y['enddate'].strftime('%Y%m%d'))
+    subset_file_info = all_file_info.loc[all_file_info['date_info']>= start_stamp].reset_index(drop =True) # | all_file_info['date_info']<= end_stamp]
+    subset_file_info = all_file_info.loc[all_file_info['date_info']<= end_stamp].reset_index(drop =True)
+    return subset_file_info
+
+# We adopted some code contributed by Joe Futrelle (WHOI): https://github.com/joefutrelle/pyifcb
 
 # make function to get datasets from urls
 def df_from_url(url):
@@ -67,22 +77,38 @@ def df_from_url(url):
     return df
 
 
-## create list of dates available for the project of interest
-link = 'https://ifcb.caloos.org/api/list_bins?dataset=' + dataset_name # this path is for the data LIST only
-metadata_link = "https://ifcb.caloos.org/timeline?dataset="
-r = requests.get(link)
-r_content = r.content
-r_content = json.loads(r_content.decode('utf-8'))
-all_file_info = pd.DataFrame.from_dict(r_content['data'], orient = 'columns')
-all_file_info['date_info'] = [int(sample[1:9]) for sample in all_file_info['pid']]
-# constrain date ranges for file download
-start_stamp=int(y['startdate'].strftime('%Y%m%d'))
-end_stamp=int(y['enddate'].strftime('%Y%m%d'))
-subset_file_info = all_file_info.loc[all_file_info['date_info']>= start_stamp].reset_index(drop =True) # | all_file_info['date_info']<= end_stamp]
-subset_file_info = all_file_info.loc[all_file_info['date_info']<= end_stamp].reset_index(drop =True)
+def metadata_dict(pid_id):
+    """
+    :param pid: the identifier for the data in each time bin displayed on the timeline in the dashboard
+    :return: dictionary with the metadata
+    """
+    url = f'{BASE_URL}api/bin/{pid_id}?include_coordinates=False'
+    r = requests.get(url)
+    assert r.ok
+    record = r.json()
+    return {
+        'datetime': record['timestamp_iso'],
+        'previous_bin': record['previous_bin_id'],
+        'next_bin': record['next_bin_id'],
+        'latitude': record['lat'],
+        'longitude': record['lng'],
+        'depth': record['depth'],
+        'instrument': record['instrument'],
+        'number_of_rois': record['num_images'],
+        'ml_analyzed': float(re.sub(' .*', '', record['ml_analyzed'])),
+        'concentration': record['concentration'],
+        'sample_type': record['sample_type'],
+        'cruise': record['cruise'],
+        'cast': record['cast'],
+        'niskin': record['niskin'],
+    }
+
+#PROCESSING HERE
+
+
 
 # use the file list to download data and store it locally
-pathname = 'https://ifcb.caloos.org/' + dataset_name + '/' # notice the different url for the data
+subset_file_info = get_df_list_IFCB (base_url = Project_source, dataset = Project_ID)
 for i in range (len(subset_file_info)):
     localpath = y['path_download'] + '/' + str(subset_file_info.loc[i, 'pid'])# create local directory to store the files
     if os.path.exists(localpath) and os.path.isdir(localpath):
@@ -91,21 +117,23 @@ for i in range (len(subset_file_info)):
     #generate features files
     features_filename = pathname + str(subset_file_info.loc[i, 'pid']) + '_features.csv'
     features_df = df_from_url(features_filename)
+    # obtain metadata
+    pid_id = str(subset_file_info.loc[i, 'pid'])
+    met_dict = metadata_dict(pid_id)
     for c in ['texture', 'Wedge', 'Ring', 'HOG', 'moment_invariant']:
         features_df = features_df[features_df.columns.drop(list(features_df.filter(regex=c)))]
-    url = metadata_link + dataset_name +'&bin='+ str(subset_file_info.loc[i, 'pid'])
-    with webdriver.Chrome(chromedriver, options=options, keep_alive=False) as driver:
-        driver.get(url)
-        time.sleep(60)  # Time needed for the webpage to be fully loaded, in seconds
-        vol_analyzed = float(re.sub("[^\d\.]", "", driver.find_element(by='id', value='stat-ml-analyzed').text)) if len(driver.find_element(by='id', value='stat-ml-analyzed').text) > 0 else pd.NA
-        depth = float(re.sub("[^\d\.]", "", driver.find_element(by='id', value='stat-depth').text)) if len(driver.find_element(by='id', value='stat-depth').text) > 0 else pd.NA
-        Latitude = float(driver.find_element(by='id', value='stat-lat').text) if len(driver.find_element(by='id', value='stat-lat').text) > 0 else pd.NA
-        Longitude = float(driver.find_element(by='id', value='stat-lon').text) if len(driver.find_element(by='id', value='stat-lon').text) > 0 else pd.NA
-        driver.quit()
-    features_df['Latitude'] = Latitude
-    features_df['Longitude'] = Longitude
-    features_df['depth'] = depth
-    features_df['vol_analyzed'] = vol_analyzed
+    #url = metadata_link + dataset_name +'&bin='+ str(subset_file_info.loc[i, 'pid'])
+    features_df['datetime'] = met_dict['datetime']
+    features_df['Latitude'] = met_dict['latitude']
+    features_df['Longitude'] = met_dict['longitude']
+    features_df['depth'] = met_dict['depth']
+    features_df['vol_analyzed'] = met_dict['ml_analyzed']
+    features_df['sample_type'] = met_dict['sample_type']
+    features_df['cruise'] = met_dict['cruise']
+    features_df['number_of_rois'] = met_dict['number_of_rois']
+    features_df['concentration'] = met_dict['concentration']
+
+
 
     #now generate dataframe for the class scores
     class_filename = pathname + str(subset_file_info.loc[i, 'pid']) + '_class_scores.csv'
