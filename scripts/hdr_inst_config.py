b1f17367b5b3d42c6fa3f2ae98bce2e54c7ade93
MarCorralesU <marco.corrales@noaa.gov>
2022-12-14 10:39:33 -0500

    listed essential parameters that connstrain IFCB image acquisition . requires pyifcb

--- scripts/hdr_inst_config.py
+++ scripts/hdr_inst_config.py
@@ -6,3 +6,18 @@ url = 'http://ifcb-data.whoi.edu/IFCB101_BigelowJun2016/D20160709T210837_IFCB101
 #with ifcb.open_url(url) as sample_bin:
     #lid = sample_bin.lid
     print('{} has {} image(s)'.format(lid, len(sample_bin.images)))
+
+# parameters to get:
+
+#  minBlobArea= sample_bin.headers['minimumBlobArea']
+
+#  PMTAhighVoltage= sample_bin.headers['PMTAhighVoltage']
+#  PMTBhighVoltage= sample_bin.headers['PMTBhighVoltage']
+#  PMTChighVoltage= sample_bin.headers['PMTChighVoltage']
+
+#  SyringeSampleVolume= sample_bin.headers['SyringeSampleVolume']
+
+#  PMTAtriggerThreshold_DAQ_MCConly= sample_bin.headers['PMTAtriggerThreshold_DAQ_MCConly']
+#  PMTBtriggerThreshold_DAQ_MCConly= sample_bin.headers['PMTBtriggerThreshold_DAQ_MCConly']
+#  PMTCtriggerThreshold_DAQ_MCConly= sample_bin.headers['PMTCtriggerThreshold_DAQ_MCConly']
+#  PMTDtriggerThreshold_DAQ_MCConly= sample_bin.headers['PMTDtriggerThreshold_DAQ_MCConly']
