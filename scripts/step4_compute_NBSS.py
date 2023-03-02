1e23110db6a05d36c3cf455de7c3a6f2d7fcb87c
MarCorralesU <103773501+MarCorralesU@users.noreply.github.com>
2023-03-02 15:54:54 -0500

    modified script structure to do NBSS calculations from data coming from one 15x15 degree cell, with the hopes of avoiding the creation of a HUGE file when all the gridded data is concatenated

--- scripts/step4_compute_NBSS.py
+++ scripts/step4_compute_NBSS.py
@@ -22,11 +22,17 @@ import cartopy.crs as ccrs
 
 instrument = input ('for which instrument do you want to calculate the size spectra? \n Enter IFCB, Zooscan or UVP ')
 day_night = input('Would you like to group data by day/night? Enter Y/N \n' )
-depth_binning  = input ('Would you like to bin the data by depth? \n Enter Y/N')
-
+depth_binning  = input ('Would you like to bin the data by depth? \n Enter Y/N \n')
+bin_loc = float(input('select the size of the spatial grid to group the data i.e for a 1x1 degree bin type 1 \n' ))
+group_by= input('input how will the data be grouped by date \n  yyyymm for month and year \n yyyy for year \n ')
+big_grid_status = input('have the gridded files already been parsed and labelled according to their large global grid? \n type Y or N \n' )
 # processing starts here
 
-file_list = proj_id_list_func(instrument, data_status ='gridded')#generate path and project ID's
+#first, break apart datasets by big global grids, to avoid making one HUGE file of all gridded datasets per instrument
+grid_list = group_gridded_files_func(instrument, already_gridded=big_grid_status)
+
+#get paths to gridded files
+file_list = proj_id_list_func(instrument, data_status ='gridded', big_grid = True)#generate path and project ID's but ONLY for parsed data
 
 path_to_config = Path('~/GIT/PSSdb/scripts/Ecotaxa_API.yaml').expanduser()
 with open(path_to_config, 'r') as config_file:
@@ -45,79 +51,90 @@ if os.path.isdir(dirpath) and len(os.listdir(dirpath)) != 0:  # and  os.path.exi
         print('Overwriting normalized biomass data file(s), please wait')
         shutil.rmtree(dirpath)
         os.mkdir(dirpath)
-        df = pd.concat(map((lambda path: (pd.read_csv(path))), file_list)).reset_index(drop=True)
-        df = df.filter(['Sample','date_bin', 'Station_location','light_cond', 'midDepthBin', 'Min_obs_depth', 'Max_obs_depth', 'Biovolume', 'midLatBin', 'midLonBin', 'Volume_imaged'], axis=1)
+        NBSS_1a_full = pd.DataFrame()
+        lin_fit_1b_full = pd.DataFrame()
+        for i in tqdm(grid_list):
+            print('grouping data and calculating NBSS for cell number ' + i)
+            file_subset = [file for file in file_list if i in file]
+            df = pd.concat(map((lambda path: (pd.read_csv(path))), file_subset)).reset_index(drop=True)
+            df = df.filter(['Sample','date_bin', 'Station_location','light_cond', 'midDepthBin', 'Min_obs_depth', 'Max_obs_depth', 'Biovolume', 'midLatBin', 'midLonBin', 'Volume_imaged'], axis=1)
+            if depth_binning == 'Y':
+                NBSS_1a, lin_fit_1b = parse_NBS_linfit_func(df, parse_by=['Station_location', 'date_bin'], light_parsing=True, depth_parsing= True, bin_loc = bin_loc, group_by = group_by)
+            if day_night == 'Y':
+                NBSS_1a, lin_fit_1b = parse_NBS_linfit_func(df, parse_by=['Station_location', 'date_bin'], light_parsing=True, bin_loc = bin_loc, group_by = group_by)
+            else:
+                NBSS_1a, lin_fit_1b = parse_NBS_linfit_func(df, parse_by=['Station_location', 'date_bin'], bin_loc = bin_loc, group_by = group_by)
 
-        if depth_binning == 'Y':
-            NBSS_1a, lin_fit_1b = parse_NBS_linfit_func(df, parse_by=['Station_location', 'date_bin'], light_parsing=True, depth_parsing= True)
-        if day_night == 'Y':
-            NBSS_1a, lin_fit_1b = parse_NBS_linfit_func(df, parse_by=['Station_location', 'date_bin'], light_parsing=True)
-        else:
-            NBSS_1a, lin_fit_1b = parse_NBS_linfit_func(df, parse_by=['Station_location', 'date_bin'])
-
+            NBSS_1a_full = pd.concat([NBSS_1a_full, NBSS_1a])
+            lin_fit_1b_full = pd.concat([lin_fit_1b_full, lin_fit_1b])
         # Save NBSS results
-        NBSS_1a.to_csv(str(dirpath) + '/' + instrument +'_NBSS_1a.csv', index=False)
-        lin_fit_1b.to_csv(str(dirpath) + '/' + instrument + '_lin_fit_1b.csv', index=False)
+        NBSS_1a_full.to_csv(str(dirpath) + '/' + instrument +'_NBSS_1a.csv', index=False)
+        lin_fit_1b_full.to_csv(str(dirpath) + '/' + instrument + '_lin_fit_1b.csv', index=False)
 
     elif replace == 'N':
         print('previous products 1a and 1b will be kept')
 
 elif not os.path.exists(dirpath):
     os.mkdir(dirpath)
-    df = pd.concat(map((lambda path: (pd.read_csv(path))), file_list)).reset_index(drop=True)
-    df = df.filter(
-        ['Sample', 'date_bin', 'Station_location', 'light_cond', 'midDepthBin', 'Min_obs_depth', 'Max_obs_depth', 'range_size_bin',
-         'sizeClasses', 'Biovolume', 'midLatBin', 'midLonBin', 'Volume_imaged'], axis=1)
+    NBSS_1a_full = pd.DataFrame()
+    lin_fit_1b_full = pd.DataFrame()
+    for i in tqdm(grid_list):
+        print('grouping data and calculating NBSS for cell number ' + i)
+        file_subset = [file for file in file_list if i in file]
+        df = pd.concat(map((lambda path: (pd.read_csv(path))), file_subset)).reset_index(drop=True)
+        df = df.filter(
+            ['Sample', 'date_bin', 'Station_location', 'light_cond', 'midDepthBin', 'Min_obs_depth', 'Max_obs_depth',
+             'Biovolume', 'midLatBin', 'midLonBin', 'Volume_imaged'], axis=1)
+        if depth_binning == 'Y':
+            NBSS_1a, lin_fit_1b = parse_NBS_linfit_func(df, parse_by=['Station_location', 'date_bin'],
+                                                        light_parsing=True, depth_parsing=True, bin_loc = bin_loc, group_by = group_by)
+        if day_night == 'Y':
+            NBSS_1a, lin_fit_1b = parse_NBS_linfit_func(df, parse_by=['Station_location', 'date_bin'],
+                                                        light_parsing=True, bin_loc = bin_loc, group_by = group_by)
+        else:
+            NBSS_1a, lin_fit_1b = parse_NBS_linfit_func(df, parse_by=['Station_location', 'date_bin'], bin_loc = bin_loc, group_by = group_by)
 
-    if depth_binning == 'Y':
-        NBSS_1a, lin_fit_1b = parse_NBS_linfit_func(df, parse_by=['Station_location', 'date_bin'], light_parsing=True,
-                                                    depth_parsing=True)
-    if day_night == 'Y':
-        NBSS_1a, lin_fit_1b = parse_NBS_linfit_func(df, parse_by=['Station_location', 'date_bin'], light_parsing=True)
-    else:
-        NBSS_1a, lin_fit_1b = parse_NBS_linfit_func(df, parse_by=['Station_location', 'date_bin'])
-
+        NBSS_1a_full = pd.concat([NBSS_1a_full, NBSS_1a])
+        lin_fit_1b_full = pd.concat([lin_fit_1b_full, lin_fit_1b])
     # Save NBSS results
-    NBSS_1a.to_csv(str(dirpath) + '/' + instrument + '_NBSS_1a.csv', index=False)
-    lin_fit_1b.to_csv(str(dirpath) + '/' + instrument + '_lin_fit_1b.csv', index=False)
+    NBSS_1a_full.to_csv(str(dirpath) + '/' + instrument + '_NBSS_1a.csv', index=False)
+    lin_fit_1b_full.to_csv(str(dirpath) + '/' + instrument + '_lin_fit_1b.csv', index=False)
 
 
-lat = lin_fit_1b['latitude']
-lon = lin_fit_1b['longitude']
-slope = lin_fit_1b['slope_mean']
-intercept_t = lin_fit_1b['intercept_mean']
+#lat = lin_fit_1b['latitude']
+#lon = lin_fit_1b['longitude']
+#slope = lin_fit_1b['slope_mean']
+#intercept_t = lin_fit_1b['intercept_mean']
 
-intercept_plot = [x*3 for x in intercept_t]
-ax = plt.axes(projection=ccrs.PlateCarree())
-plt.gca().coastlines('50m')
-g1=ax.gridlines(draw_labels=True)
-g1.xlines = False
-g1.ylines = False
-plt.scatter(lon, lat, label=None, c=slope, cmap='viridis', s=intercept_plot, linewidth=0, alpha=0.5, transform=ccrs.PlateCarree())
-plt.gca().coastlines('50m')
-ax.set_extent([-180, 180, -90, 90])
+#intercept_plot = [x*3 for x in intercept_t]
+#ax = plt.axes(projection=ccrs.PlateCarree())
+#plt.gca().coastlines('50m')
+#g1=ax.gridlines(draw_labels=True)
+#g1.xlines = False
+#g1.ylines = False
+#plt.scatter(lon, lat, label=None, c=slope, cmap='viridis', s=intercept_plot, linewidth=0, alpha=0.5, transform=ccrs.PlateCarree())
+#plt.gca().coastlines('50m')
+#ax.set_extent([-180, 180, -90, 90])
 #ax.xlabel('longitude')
 #ax.ylabel('latitude')
-plt.colorbar(label='slope', orientation='horizontal', anchor=(0.5, 1))
+#plt.colorbar(label='slope', orientation='horizontal', anchor=(0.5, 1))
 #ax.clim(min(slope), max(slope))
 
 
-labels = [str(np.round(min(intercept_t), decimals=2)),
-          str(np.round(st.median(intercept_t), decimals=2)),
-          str(np.round(max(intercept_t), decimals=2))]
+#labels = [str(np.round(min(intercept_t), decimals=2)),str(np.round(st.median(intercept_t), decimals=2)), str(np.round(max(intercept_t), decimals=2))]
 
-for n, area in enumerate([18, 28.5, 73.5]):
-    plt.scatter([], [], c='k', alpha=0.3, s=area, label=labels[n], transform=ccrs.PlateCarree())
+#for n, area in enumerate([18, 28.5, 73.5]):
+    #plt.scatter([], [], c='k', alpha=0.3, s=area, label=labels[n], transform=ccrs.PlateCarree())
 
-plt.legend(bbox_to_anchor=(0.75, 0), ncol = 3, scatterpoints=1, frameon=False,
-           labelspacing=1, title='intercept')
+#plt.legend(bbox_to_anchor=(0.75, 0), ncol = 3, scatterpoints=1, frameon=False,
+           #labelspacing=1, title='intercept')
 
-figname = 'step4_slopes_intercept_updated_firstrelease' + instrument + '.pdf'
+#figname = 'step4_slopes_intercept_updated_firstrelease' + instrument + '.pdf'
 
-path_to_config = Path('~/GIT/PSSdb/scripts/Ecotaxa_API.yaml').expanduser()
-with open(path_to_config, 'r') as config_file:
-    cfg = yaml.safe_load(config_file)
+#path_to_config = Path('~/GIT/PSSdb/scripts/Ecotaxa_API.yaml').expanduser()
+#with open(path_to_config, 'r') as config_file:
+    #cfg = yaml.safe_load(config_file)
 
-savepath = Path(cfg['git_dir']).expanduser() / 'figures' / figname
+#savepath = Path(cfg['git_dir']).expanduser() / 'figures' / figname
 
 #plt.savefig(savepath)
