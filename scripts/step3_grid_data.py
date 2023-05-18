076ce8db1f7b6c4013bc1777533d0e26b8b3a498
MarCorralesU <103773501+MarCorralesU@users.noreply.github.com>
2023-05-18 18:36:59 -0400

    removed extra index columns

--- scripts/step3_grid_data.py
+++ scripts/step3_grid_data.py
@@ -101,16 +101,16 @@ if os.path.isdir(dirpath) and len(os.listdir(dirpath)) != 0:  # and  os.path.exi
                     #n_del_files += 1
                     #continue
             if (instrument == 'Zooscan') or (instrument == 'UVP'):
-                df = depth_parsing_func(df, instrument).reset_index(drop=True)
+                df = depth_parsing_func(df, instrument)
                 if len(df) == 0:
                     print('no data left after restricting depths to less than 200 meters in ' + filename)
                     n_del_files += 1
                     continue
             elif instrument == 'IFCB':
-                df = depth_parsing_func(df, instrument).reset_index(drop=True)
+                df = depth_parsing_func(df, instrument)
                 df = df.replace(np.nan, '')
                 sampling_type_to_remove = ['test', 'exp', 'junk', 'culture']
-                df = df[~df.Sampling_type.str.contains('|'.join(sampling_type_to_remove))].reset_index(drop=True)
+                df = df[~df.Sampling_type.str.contains('|'.join(sampling_type_to_remove))]
                 df = df.replace('', np.nan)
                 if len(df) == 0:
                     print('no data left after restricting depths to less than 200 meters in ' + filename)
@@ -179,17 +179,17 @@ elif not os.path.exists(dirpath):
         df = df.dropna(subset=['Latitude']).reset_index(drop=True)
         df = df[df['Area'] != 0].reset_index(drop=True)
         if (instrument == 'Zooscan') or (instrument == 'UVP'):
-            df = depth_parsing_func(df, instrument).reset_index(drop=True)
+            df = depth_parsing_func(df, instrument)
             if len(df) == 0:
                 print('no data left after restricting depths to less than 200 meters in ' + filename)
                 n_del_files += 1
                 continue
         elif instrument == 'IFCB':
-            df = depth_parsing_func(df, instrument).reset_index(drop=True)
+            df = depth_parsing_func(df, instrument)
             df = df.replace(np.nan, '')
             sampling_type_to_remove = ['test', 'exp', 'junk', 'culture']
             df = df[~df.Sampling_type.str.contains('|'.join(sampling_type_to_remove))]
-            df = df.replace('', np.nan).reset_index(drop=True)
+            df = df.replace('', np.nan)
             if len(df) == 0:
                 print('no data left after restricting depths to less than 200 meters in ' + filename)
                 n_del_files += 1
