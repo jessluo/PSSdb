168bec67efc043b7304ffd328fdd306f2a8db4d5
MarCorralesU <103773501+MarCorralesU@users.noreply.github.com>
2023-07-17 19:08:14 -0400

    added code for figures 3,4 and 5

--- scripts/script_pssdb_firstrelease.py
+++ scripts/script_pssdb_firstrelease.py
@@ -112,12 +112,101 @@ theme(axis_ticks_direction="inout",
                       panel_grid=element_blank(),
                       panel_background=element_rect(fill='white'),
                       panel_border=element_rect(color='#222222'),
-                      #legend_title=element_text(family="serif", size=10),
-                      #legend_text=element_text(family="serif", size=10),
+                      legend_title=element_text(family="serif", size=10),
+                      legend_text=element_text(family="serif", size=10),
                       axis_title=element_text(family="serif", size=10),
                       axis_text_x=element_text(family="serif", size=10),
                       axis_text_y=element_text(family="serif", size=10, rotation=90),
                       plot_background=element_rect(fill='white'),strip_background=element_rect(fill='white'))).draw(show=False, return_ggplot=True)
 
-plot[0].set_size_inches(6.5,2.8)
+plot[0].set_size_inches(13,3.1)
+plt.tight_layout()
 plot[0].savefig(fname='{}/GIT/PSSdb/figures/first_datapaper/Fig_4.1_slopes_NBSS.pdf'.format(str(Path.home())), dpi=600)
+plt.close()
+
+# Intercept
+
+world = gpd.read_file(gpd.datasets.get_path("naturalearth_lowres"))
+world_polygon = pd.concat([pd.concat([pd.DataFrame({'Country': np.repeat(country_polygon, pd.DataFrame(polygon[0]).shape[0])}),pd.DataFrame(polygon[0], columns=['Longitude', 'Latitude'])], axis=1) if pd.DataFrame(polygon[0]).shape[1] > 1 else pd.concat([pd.DataFrame({'Country': np.repeat(country_polygon, pd.DataFrame(polygon).shape[0])}),pd.DataFrame(polygon, columns=['Longitude', 'Latitude'])], axis=1) for country, region in zip(world.name, world.geometry) for country_polygon, polygon in zip([str(country) + "_" + str(poly) for poly in np.arange(len(mapping(region)['coordinates'])).tolist()], mapping(region)['coordinates'])], axis=0)
+#df_summary=df.groupby(['Instrument','latitude','longitude']).agg({'N':'sum'}).reset_index().rename(columns={'N':'count'})
+plot=(ggplot(data=df)+
+  facet_wrap('~Instrument', nrow=1)+
+  geom_point( aes(x="longitude",y="latitude",fill='intercept_mean'),shape='H',color='#{:02x}{:02x}{:02x}{:02x}'.format(255, 255 , 255,0),alpha=1, size=1) +
+  coord_cartesian(expand = 0)+
+ geom_polygon(data=world_polygon, mapping=aes(x='Longitude', y='Latitude', group='Country'), fill='black', color='black') +
+ labs(x=r'Longitude ($^{\circ}$E)', y=r'Latitude ($^{\circ}$N)', color=r'Mean slope') +
+ scale_fill_cmap(limits=[5,25]) +
+ #scale_fill_gradientn(trans='log10',colors=palette)+
+theme(axis_ticks_direction="inout",
+                      panel_grid=element_blank(),
+                      panel_background=element_rect(fill='white'),
+                      panel_border=element_rect(color='#222222'),
+                      legend_title=element_text(family="serif", size=10),
+                      legend_text=element_text(family="serif", size=10),
+                      axis_title=element_text(family="serif", size=10),
+                      axis_text_x=element_text(family="serif", size=10),
+                      axis_text_y=element_text(family="serif", size=10, rotation=90),
+                      plot_background=element_rect(fill='white'),strip_background=element_rect(fill='white'))).draw(show=False, return_ggplot=True)
+
+plot[0].set_size_inches(13,3.1)
+plt.tight_layout()
+plot[0].savefig(fname='{}/GIT/PSSdb/figures/first_datapaper/Fig_4.2_intercepts_NBSS.pdf'.format(str(Path.home())), dpi=600)
+plt.close()
+
+#R2
+
+world = gpd.read_file(gpd.datasets.get_path("naturalearth_lowres"))
+world_polygon = pd.concat([pd.concat([pd.DataFrame({'Country': np.repeat(country_polygon, pd.DataFrame(polygon[0]).shape[0])}),pd.DataFrame(polygon[0], columns=['Longitude', 'Latitude'])], axis=1) if pd.DataFrame(polygon[0]).shape[1] > 1 else pd.concat([pd.DataFrame({'Country': np.repeat(country_polygon, pd.DataFrame(polygon).shape[0])}),pd.DataFrame(polygon, columns=['Longitude', 'Latitude'])], axis=1) for country, region in zip(world.name, world.geometry) for country_polygon, polygon in zip([str(country) + "_" + str(poly) for poly in np.arange(len(mapping(region)['coordinates'])).tolist()], mapping(region)['coordinates'])], axis=0)
+#df_summary=df.groupby(['Instrument','latitude','longitude']).agg({'N':'sum'}).reset_index().rename(columns={'N':'count'})
+plot=(ggplot(data=df)+
+  facet_wrap('~Instrument', nrow=1)+
+  geom_point( aes(x="longitude",y="latitude",fill='r2_mean'),shape='H',color='#{:02x}{:02x}{:02x}{:02x}'.format(255, 255 , 255,0),alpha=1, size=1) +
+  coord_cartesian(expand = 0)+
+ geom_polygon(data=world_polygon, mapping=aes(x='Longitude', y='Latitude', group='Country'), fill='black', color='black') +
+ labs(x=r'Longitude ($^{\circ}$E)', y=r'Latitude ($^{\circ}$N)', color=r'Mean slope') +
+ scale_fill_cmap(limits=[0.85,0.99]) +
+ #scale_fill_gradientn(trans='log10',colors=palette)+
+theme(axis_ticks_direction="inout",
+                      panel_grid=element_blank(),
+                      panel_background=element_rect(fill='white'),
+                      panel_border=element_rect(color='#222222'),
+                      legend_title=element_text(family="serif", size=10),
+                      legend_text=element_text(family="serif", size=10),
+                      axis_title=element_text(family="serif", size=10),
+                      axis_text_x=element_text(family="serif", size=10),
+                      axis_text_y=element_text(family="serif", size=10, rotation=90),
+                      plot_background=element_rect(fill='white'),strip_background=element_rect(fill='white'))).draw(show=False, return_ggplot=True)
+
+plot[0].set_size_inches(13,3.1)
+plt.tight_layout()
+plot[0].savefig(fname='{}/GIT/PSSdb/figures/first_datapaper/Fig_4.3_regress_coeff_NBSS.pdf'.format(str(Path.home())), dpi=600)
+plt.close()
+
+#Figure 5. Parameters by latitude
+
+#slope
+sns.lineplot(data=df, x='latitude', y='slope_mean', hue='Instrument', palette=['red', 'cyan', 'green'], ci= None)
+sns.despine(top = True, right = True)
+plt.ylabel( r'Mean slope ( dm$^{-3}$ $\mu$m$^{-3}$)')
+plt.xlabel(r'Latitude ($\degree$N)')
+#plt.set_size_inches(6.5,2.8)
+plt.savefig(fname='{}/GIT/PSSdb/figures/first_datapaper/Fig_5.1_slope_lat.pdf'.format(str(Path.home())), dpi=600)
+plt.close()
+
+#intercept
+sns.lineplot(data=df, x='latitude', y='intercept_mean', hue='Instrument', palette=['red', 'cyan', 'green'], ci= None)
+sns.despine(top = True, right = True)
+plt.ylabel( r'Mean Intercept ( $\mu$m$^{3}$ dm$^{-3}$ $\mu$m$^{-3}$)')
+plt.xlabel(r'Latitude ($\degree$N)')
+#plt.set_size_inches(6.5,2.8)
+plt.savefig(fname='{}/GIT/PSSdb/figures/first_datapaper/Fig_5.2_intercept_lat.pdf'.format(str(Path.home())), dpi=600)
+plt.close()
+
+#R2
+sns.lineplot(data=df, x='latitude', y='r2_mean', hue='Instrument', palette=['red', 'cyan', 'green'], ci= None)
+sns.despine(top = True, right = True)
+plt.ylabel( r'Mean R$^{2}$')
+plt.xlabel(r'Latitude ($\degree$N)')
+#plt.set_size_inches(6.5,2.8)
+plt.savefig(fname='{}/GIT/PSSdb/figures/first_datapaper/Fig_5.3_R2_lat.pdf'.format(str(Path.home())), dpi=600)
+plt.close()
