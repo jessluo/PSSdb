10bf2fcb6814baf456fab2566cdbf71d930098de
MarCorralesU <103773501+MarCorralesU@users.noreply.github.com>
2023-08-23 17:20:53 -0400

    removed old functions and added rounding to ECD size classes

--- scripts/funcs_NBS.py
+++ scripts/funcs_NBS.py
@@ -18,57 +18,6 @@ from operator import attrgetter
 
 
 
-
-
-def group_gridded_files_func_old(instrument, already_gridded= 'N'):
-    """
-    Objective: assign a label to each gridded dataset, so when the compilation of files occurs, it is only done with a group of data and prevents
-    the creation of a huge file
-    """
-    #first, create series of numbers that break the globe into 15x15 degree cells:
-    lat_left = np.arange(-90, 90+15, 15, dtype=int)
-
-    lat_right = np.arange(-75,105 + 15, 15, dtype=int)
-    lat_int = pd.IntervalIndex.from_arrays(lat_left, lat_right)
-    lon_left = np.arange(-180, 180+15, 15, dtype=int)
-    lon_right = np.arange(-165, 195+15, 15, dtype=int)
-    lon_int = pd.IntervalIndex.from_arrays(lon_left, lon_right)
-    grid_list = []
-    # create a dictionary that has unique keys for each grid interval
-    #grid_dict = {}
-    #for x in range(0, len(lon_int)):
-        #for y in range(0, len(lat_int)):
-            #grid_dict[(str(x) + '_' + str(y))] = {'lon': lon_int[x], 'lat': lat_int[y]} ## however, this might not be necessary
-    # try now to assing lat & lon grid numbers to the dataframe directly
-    file_list = proj_id_list_func(instrument, data_status='gridded')
-    for i in tqdm(file_list):
-        if already_gridded == 'N':
-            print('generating subsets of data for 15 degree grids from ' + i)
-        else:
-            print('extracting big grid labels for subsetting from ' + i)
-        filename = i.split('/')[-1]
-        dirpath = i.replace(filename, '')
-        df = pd.read_csv(i, header = 0)
-        df['lat_grid'] = None
-        df['lon_grid'] = None
-        for la in df['midLatBin'].unique():
-            df.loc[df['midLatBin'] == la, 'lat_grid']= str(lat_int[lat_int.contains(la) == True][0].left) + '-' + str(lat_int[lat_int.contains(la) == True][0].right)
-        for lo in df['midLonBin'].unique():
-            df.loc[df['midLonBin'] == lo, 'lon_grid'] = str(lon_int[lon_int.contains(lo) == True][0].left) + '-' + str(lon_int[lon_int.contains(lo) == True][0].right)
-        # combine these numbers to form a grid identifier
-        df['grid_id'] = df.lat_grid.astype(str).str.cat(df.lon_grid.astype(str), sep='_')
-        df_subgrouped = [x for _, x in df.groupby('grid_id')]
-        for s in df_subgrouped:
-            grid_list.append(str(s['grid_id'].unique()[0]))
-            if already_gridded == 'N':
-                s.to_csv(str(dirpath) + 'grid_N_'+str(s['grid_id'].unique()[0]) + '_'+ filename, index=False)
-            #os.remove(i)
-    grid_list_unique = [*set(grid_list)]
-    grid_list_unique = ['N_' + s for s in grid_list_unique]
-    return grid_list_unique
-
-
-
   #6.1 by size bins. Inputs: a column of the dataframe with biovolume, and the number of log bins to use.
     #returns two lists : the sizeClasses bins (categorical) and range_size_bins (float)
 def size_binning_func(df_subset):
@@ -188,7 +137,7 @@ def NB_SS_func(df_binned, df_bins, light_parsing = False, depth_parsing = False,
             #average NB for each date and station bin, from NB that come from different net tows.
             NBS_biovol_df= NBS_biovol_df.groupby(['date_bin', 'Station_location','sizeClasses','midLatBin', 'midLonBin', 'Min_obs_depth', 'Max_obs_depth']).apply(lambda x: pd.Series({
                                                                                                                      'Biovolume_mean': x.Biovolume_mean.mean(),
-                                                                                                                     'ECD_mean': ((x.size_class_mid.unique()[0]*6)/m.pi)**(1./3.), #in micrometers
+                                                                                                                     'ECD_mean': np.round(((x.size_class_mid.unique()[0]*6)/m.pi)**(1./3.), decimals = 2), #in micrometers
                                                                                                                      'ROI_number_sum': x.ROI_number_sum.unique()[0],
                                                                                                                      'ROI_abundance_mean': x.ROI_abundance.mean(),
                                                                                                                      'size_class_mid': x.size_class_mid.unique()[0],
